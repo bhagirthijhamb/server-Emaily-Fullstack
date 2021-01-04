d866f77d9d68b4c647d3b391143e86a88b5cd249
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-01-04 09:25:11 -0500

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+}
+
+// mongodb+srv://bhagirthi:bhagirthi123@cluster0.8ch6m.mongodb.net/emaily-prod?retryWrites=true&w=majority
