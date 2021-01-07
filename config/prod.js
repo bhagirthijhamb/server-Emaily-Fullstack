096cc9cb203f0823222f64af412388d0fff26c5d
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-01-06 22:44:39 -0500

    completed Server side survey creation

--- config/prod.js
+++ config/prod.js
@@ -5,7 +5,8 @@ module.exports = {
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-  sendGridKey: process.env.SEND_GRID_KEY
+  sendGridKey: process.env.SEND_GRID_KEY,
+  redirectDomain: process.env.REDIRECT_DOMAIN
 }
 
 // mongodb+srv://bhagirthi:bhagirthi123@cluster0.8ch6m.mongodb.net/emaily-prod?retryWrites=true&w=majority
