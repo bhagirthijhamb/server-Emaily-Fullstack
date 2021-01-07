ce6b499ce233ac67c9c98395de2f6aea1ecf605b
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-01-07 11:07:43 -0500

    added Dashboard component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,11 +3,12 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Header from './Header';
 import Landing from './Landing';
+import Dashboard from './Dashboard';
 import * as actions from './../actions';
 
 // Dummy components
 // const Header = () => <h2>Header</h2>
-const Dashboard = () => <h2>Dashboard</h2>
+// const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 // const Landing = () => <h2>Landing</h2>
 
